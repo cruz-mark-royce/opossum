c1f2aa8eb653e239e45d8561ccd3edd9dc8190d0
Cruz <nunez.a.cruz@gmail.com>
2015-06-11 17:46:29 -0400

    Add user validations #shipit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
-end
+  validates :email, presence: true, uniqueness: true, format: {with: /\w+@\w+.\w+/}
+  validates :password, length: { in: 4..20 }end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
+require 'minitest/pride'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should require email" do
+    user1 = User.new(password: "notapassword")
+    user2 = User.new(password: "notapassword", email: "not@email.com")
+    refute user1.save
+    assert user2.save
+  end
+
+  test "should require password with length from 4-20" do
+    user0 = User.new(email: "not@email.com")
+    user1 = User.new(email: "not@email.com", password: "no")
+    user2 = User.new(email: "not@email.com", password: "just_right")
+    user3 = User.new(email: "not@email.com", password: "_____too_____long_____")
+    refute user0.save
+    refute user1.save
+    assert user2.save
+    refute user3.save
+  end
 end
