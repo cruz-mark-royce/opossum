07ea570035169232eb7c9e2527d2420afbcf6843
Cruz <nunez.a.cruz@gmail.com>
2015-06-12 12:38:25 -0400

    Functional Edit Survey Page #shipit

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,9 +3,11 @@ class Question < ActiveRecord::Base
   validates :survey_id, presence: true
   validate :valid_question_type
   validates :value, presence: true
-  validates :order, numericality: { only_integer: true }, uniqueness: {scope: :survey_id}
+  validates :order, numericality: { only_integer: true }, uniqueness: {scope: :survey_id}, :on => :create
 
-  private def valid_question_type
+  private
+
+  def valid_question_type
     valids = ['boolean', 'string', 'text']
     unless valids.include?(question_type)
       errors.add(:question_type, "is invalid")
