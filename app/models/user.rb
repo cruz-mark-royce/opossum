5f3ba42405fa29e470bdb98f99d44c8f3904a7df
Cruz <nunez.a.cruz@gmail.com>
2015-06-11 20:31:04 -0400

    Add naem validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: {with: /\w+@\w+.\w+/}
   validates :password, length: { in: 4..20 }end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,18 +2,25 @@ require 'test_helper'
 require 'minitest/pride'
 
 class UserTest < ActiveSupport::TestCase
+  test "should require name" do
+    user1 = User.new(email: "not@email.com", password: "just_right")
+    user2 = User.new(email: "n0t@email.com", password: "just_right", name: "name")
+    refute user1.save
+    assert user2.save
+  end
+
   test "should require email" do
-    user1 = User.new(password: "notapassword")
-    user2 = User.new(password: "notapassword", email: "not@email.com")
+    user1 = User.new(name: "name", password: "notapassword")
+    user2 = User.new(name: "name", password: "notapassword", email: "not@email.com")
     refute user1.save
     assert user2.save
   end
 
   test "should require password with length from 4-20" do
-    user0 = User.new(email: "not@email.com")
-    user1 = User.new(email: "not@email.com", password: "no")
-    user2 = User.new(email: "not@email.com", password: "just_right")
-    user3 = User.new(email: "not@email.com", password: "_____too_____long_____")
+    user0 = User.new(name: "name", email: "not@email.com")
+    user1 = User.new(name: "name", email: "not@email.com", password: "no")
+    user2 = User.new(name: "name", email: "not@email.com", password: "just_right")
+    user3 = User.new(name: "name", email: "not@email.com", password: "_____too_____long_____")
     refute user0.save
     refute user1.save
     assert user2.save
