a86c0d27b0fb604f5c32e6addabd7fc1a05d4462
Cruz <nunez.a.cruz@gmail.com>
2015-06-12 13:15:41 -0400

    Add question_type dropdown and update reject_if

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,7 @@
 class Survey < ActiveRecord::Base
   belongs_to :user
   has_many :questions
-  accepts_nested_attributes_for :questions, reject_if: :all_blank
+  accepts_nested_attributes_for :questions,
+      reject_if: proc { |attributes| attributes['order'].blank? && attributes['value'].blank? }
   validates :title, presence: true
 end
