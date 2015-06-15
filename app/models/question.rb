c26c257a65b2cd0420aa78f358a10606759ab6f6
Cruz <nunez.a.cruz@gmail.com>
2015-06-15 11:35:26 -0400

    Add answer saving functionality

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
 
-  attr_writer :answers_attributes
-
   has_many :answers
-  accepts_nested_attributes_for :answers
 
   validates :survey_id, presence: true
   validate :valid_question_type
